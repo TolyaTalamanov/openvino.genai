1a7cd3f9d03c56d900511802174f4fc1ce9e6d75
TolyaTalamanov <anatoliy.talamanov@intel.com>
2024-06-21 16:53:59 +0000

    Snapshot for experiments

--- samples/cpp/chat_sample/chat_sample.cpp
+++ samples/cpp/chat_sample/chat_sample.cpp
@@ -4,37 +4,23 @@
 #include "openvino/genai/llm_pipeline.hpp"
 
 int main(int argc, char* argv[]) try {
-    if (2 != argc) {
-        throw std::runtime_error(std::string{"Usage: "} + argv[0] + " <MODEL_DIR>");
-    }
-    std::string prompt;
+    std::string accumulated_str = "";
+
     std::string model_path = argv[1];
+    std::string device_name = argv[2];
+    std::string prompt = argv[3];
 
-    std::string device = "CPU";  // GPU can be used as well
-    ov::genai::LLMPipeline pipe(model_path, "CPU");
-    
-    ov::genai::GenerationConfig config;
-    config.max_new_tokens = 100;
-    std::function<bool(std::string)> streamer = [](std::string word) { 
-        std::cout << word << std::flush;
-        // Return flag correspods whether generation should be stopped.
-        // false means continue generation.
-        return false; 
-    };
+    ov::genai::LLMPipeline pipe(model_path, device_name);
+    ov::genai::GenerationConfig config = pipe.get_generation_config();
 
-    pipe.start_chat();
-    for (;;) {
-        std::cout << "question:\n";
-        
-        std::getline(std::cin, prompt);
-        if (prompt == "Stop!") 
-            break;
+    config.temperature = 0.7;
+    config.do_sample = false;
+    config.top_p = 0.95;
+    config.top_k = 40;
+    config.max_new_tokens = 15;
 
-        pipe.generate(prompt, config, streamer);
-        
-        std::cout << "\n----------\n";
-    }
-    pipe.finish_chat();
+    std::function<bool(std::string)> streamer = [](std::string word) { std::cout << word << std::flush; return false; };
+    pipe.generate(prompt, config, streamer);
 } catch (const std::exception& error) {
     std::cerr << error.what() << '\n';
     return EXIT_FAILURE;
